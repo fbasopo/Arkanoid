1d68b1f520f9d8e4271006877768ad04743ed11d
fbasopo <36536883+fbasopo@users.noreply.github.com>
2018-09-14 16:29:05 +0200

    Add files via upload

---
+++ arkanoid/arkanoid/Ball.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace arkanoid
+{
+    class Ball
+    {
+        //Game theGame;
+        double velX = 15, velY = 15;   // initialised velocities for the ball
+        Image img;      
+        
+        // img represents the ball
+        //This is to check whether the ball is within the canvas
+        //Canvas canvasP;
+        private void moveX(Canvas canvasP)  // the method to move the ball in the x axis
+        {
+            //Ball Nball = new Ball();
+            double ballX = Canvas.GetLeft(img) + velX;  // it is to move the ball to the horizontal
+            Canvas.SetLeft(img, ballX + 1);
+            if(ballX < 0 || ballX + img.ActualWidth > canvasP.ActualWidth && velX > 0) // if the ball hits the canvas 
+            {
+                velX = -velX;                                                          // chang ethe direction
+            }
+        }
+
+        public Point GetPosition()
+        {
+            Point ballY = new Point(Canvas.GetLeft(img), Canvas.GetTop(img));
+            return ballY;
+        }
+
+        private void moveY(Canvas canvasP)
+        {
+            double ballY = Canvas.GetTop(img) + velY;   // get the position and move it velY up
+            Canvas.SetTop(img, ballY + 1);              // move the ball up
+            if(ballY < 0)   // if the ball hits the top and bottom of the canvas
+            {
+                velY = -velY;                // change the direction
+            }
+        }
+        public Ball(Image img)          // the constructor for the ball
+        {
+            this.img = img;             
+            //canvasP = play;
+        }
+
+        public void update(Canvas canvas)
+        {
+            moveX(canvas);                    //method in the ball class to move the ball horizontally
+            moveY(canvas);                    //method in the ball class to movethe ball vertically
+        }
+
+        public bool LeavesArea(Canvas canvas)
+        {
+            if (Canvas.GetTop(img) + img.ActualHeight >= canvas.ActualHeight)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void bounce()
+        {
+            velY = -velY;
+        }
+       
+    }
+
+}
+    
+
---
+++ arkanoid/arkanoid/TheBoard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+
+namespace arkanoid
+{
+    class TheBoard
+    {
+        Image img;
+        Canvas canvas;
+
+        public void move_board(Canvas canvasP, Image img)
+        {
+            //double x = Canvas.GetLeft(img);
+
+        }
+        public TheBoard(Image img)   // the constructor for the board 
+        {
+            this.img = img;
+
+        }
+        bool activate = false;    // initialise the activate of the mouse
+        Point point;
+        
+        //private void paddle_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (activate)             // if activated
+        //    {
+        //        double left = Canvas.GetLeft(img) + Mouse.GetPosition(img).X - point.X; // get the position of the img relative to the canvas and add it to the position of the mouse with the image
+        //        Canvas.SetLeft(img, left); // move the mouse horizontally
+        //    }
+        //}
+
+        public void Activate()
+        {
+            activate = true;
+            point = Mouse.GetPosition(img);    // mouse must get the position of the img
+            Mouse.Capture(img);                // the mouse must capture the position of the image
+        }
+
+        public void move()
+        {
+            if (activate)
+            {
+                double left = Canvas.GetLeft(img) + Mouse.GetPosition(img).X - point.X;
+                Canvas.SetLeft(img, left);
+                double x = Canvas.GetRight(img);
+                //point = Mouse.GetPosition(paddle);
+            }
+        }
+        public bool IntersectsWith(Point p)
+        {
+            if (p.Y >= Canvas.GetTop(img))  // if the ball hits the top and bottom of the canvas
+            {
+                if (p.X > Canvas.GetLeft(img) && p.X < Canvas.GetLeft(img) + img.ActualWidth) //this if statement to know when to bounce off the board
+                {     
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+    
+
